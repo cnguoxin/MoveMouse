6dbdca05d908cc7712d1ad657791ac4f5c8326a2
cnguoxin <cn.guoxin@outlook.com>
2023-11-18 13:10:28 +0800

    time print update

--- moveMouse.py
+++ moveMouse.py
@@ -4,14 +4,16 @@ import pyautogui
 
 print("have fun")
 sTime = time.time()
+printTime = 0
 while True:
   time.sleep(5)
   mTime = (int)(time.time() - sTime)  
   hour = mTime // 3600
   min = mTime % 3600 // 60
   sec = mTime % 60
-  if (min % 5 == 0) and (min > 0):
+  if (min % 1 == 0) and (min > 0) and (printTime != min):
     print("you have been touching fish for", hour, "h", min, "min", sec, "s")
+    printTime = min
   try:
     pyautogui.moveTo(x=random.randint(100, 900), y=random.randint(100, 900))
     pyautogui.press("ctrl")
